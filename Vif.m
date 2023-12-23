c523bed7584ddbcacd7d0636d6abf19ecddd6190
SimonBarta <simabarta@gmail.com>
2023-12-23 11:27:26 +0100

    Uploading pictures

--- Vif.m
+++ Vif.m
@@ -1,6 +1,2 @@
 clear all
-ref = imread('Barvy.jpg'); %loading ref image
-jp2 = imread('Barvy.jp2'); %loading comprimaied image
-png = imread("Barvy.png"); %loading comprimaied image
-heic = imread('Barvy.heic'); %loading comprimaied imege
-heic = permute(heic, [2 1 3]); %making same dimension of loaded image as ref image have
+ViF = vif('Barvy.jpg')
